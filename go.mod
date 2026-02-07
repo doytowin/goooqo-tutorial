d9ee88bff58e110758d81bf8429dee188195ea49
Forb Yuan <f0rb@163.com>
2026-02-07 17:07:36 +0800

    REST for t_user

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module goooqo-tutorial
 
 go 1.23
 
-require github.com/mattn/go-sqlite3 v1.14.33
+require (
+	github.com/doytowin/goooqo/core v0.3.0
+	github.com/doytowin/goooqo/rdb v0.3.0
+	github.com/doytowin/goooqo/web v0.3.0
+	github.com/mattn/go-sqlite3 v1.14.33
+)
+
+require (
+	github.com/doytowin/goooqo/test v0.2.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"database/sql"
 	"net/http"
 
+	"github.com/doytowin/goooqo/core"
+	"github.com/doytowin/goooqo/rdb"
+	"github.com/doytowin/goooqo/web"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "./test.db")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	db, _ := sql.Open("sqlite3", "./test.db")
+
+	UserDataAccess := rdb.NewDataAccess[UserEntity](db)
+	web.BuildRestService[UserEntity, UserQuery]("/user/", UserDataAccess)
 
 	http.ListenAndServe(":9090", nil)
+}
+
+type UserEntity struct {
+	core.Int64Id
+	Name  *string `json:"name,omitempty"`
+	Score *int    `json:"score,omitempty"`
+	Memo  *string `json:"memo,omitempty"`
+}
+
+type UserQuery struct {
+	core.PageQuery
 }
